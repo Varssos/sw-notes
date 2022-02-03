12bda7f25d7db68d92bf829b63c8b45158367da0
Sebastian Wach <sebastianwach22@gmail.com>
2022-02-03 13:29:33 +0100

    Changed footnotes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'SW notes'
+copyright = '2022, SW'
+author = 'SW'
 
 release = '0.1'
 version = '0.1.0'
